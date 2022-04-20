a438960cd777c7afd9257d92e3833e7dbf06b903
Eugene Oh <eoh@nianticlabs.com>
2022-04-20 10:53:08 -0700

    adding graph for mean decrease in impurity, styling adjustments

--- app.py
+++ app.py
@@ -22,57 +22,105 @@ app = Dash(__name__,external_stylesheets=[dbc.themes.SPACELAB])
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    "Feature Name": ['EXT_SOURCE_3',
+                     'EXT_SOURCE_1',
+                     'EXT_SOURCE_2',
+                     'DAYS_REGISTRATION',
+                     'DAYS_BIRTH',
+                     'DAYS_EMPLOYED',
+                     'DAYS_LAST_PHONE_CHANGE',
+                     'YEARS_BEGINEXPLUATATION_MEDI',
+                     'LANDAREA_MODE',
+                     'AMT_CREDIT',
+                     'COMMONAREA_MODE',
+                     'LIVINGAPARTMENTS_AVG'],
+    "Mean Decrease in Impurity": [
+        0.05709844694331767,
+        0.040486775913126785,
+        0.02898956120671305,
+        0.025508123803522204,
+        0.023971109383082355,
+        0.022424636185079692,
+        0.018719973658598222,
+        0.017385638068831873,
+        0.01726286263265718,
+        0.016982041510066038,
+        0.016597071037180035,
+        0.01649704056420024,
+    ]
 })
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig = px.bar(
+    df,
+    x="Feature Name",
+    y="Mean Decrease in Impurity",
+    barmode="group",
+    title="Mean Decrease in Impurity for Top 12 Features",
+    height=650
+)
 
 stock_data = px.data.stocks(indexed=True)-1
 area_figs = px.area(stock_data, facet_col="company", facet_col_wrap=2)
 
-app.layout = html.Div([
-                html.Div([html.H1('Team 039 Project Dashboard: Home Credit Default Risk Analysis',style={'textAlign': 'center'})]),
-       html.Div(children=[dcc.Tabs([
-        dcc.Tab(label='Project Analysis', children=[
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    ),
-
-    dcc.Dropdown(
-        id="ticker",
-        options=["AMZN", "FB", "NFLX"],
-        value="AMZN",
-        clearable=False,
-    ),
-    dcc.Graph(id="time-series-chart"),
-
-    dcc.Graph(
-        id='areas',
-        figure=area_figs,
-    )
-]),
-
-dcc.Tab(label='Dummy Sliders', children=[
-        html.Div(html.H6('Here you can put the undersampling model to the test. There are three features below that you can manipulate and see how they effect your liklihood of defaulting. Try putting in your numbers and see how it goes! \nThe median values from the dataset are displayed as placeholders for reference.')),
-        (html.Div(children=['AMT_CREDIT: Credit amount of the loan (must be positive)',html.Br(),dcc.Input(id="AMT_CREDIT", type="number", placeholder=585000,min=0)])),
-        html.Br(),
-        (html.Div(['DAYS_BIRTH: Client\'s age in days at the time of application',html.Br(),dcc.Input(id="DAYS_BIRTH", type="number", placeholder=13883.5,min=0)])),
-        html.Br(),
-       (html.Div(['DAYS_EMPLOYED: How many days before the application the person started current employment',html.Br(),dcc.Input(id="DAYS_EMPLOYED", type="number", placeholder=1680.5,min=0)])),
-       html.Div(dbc.Button("Run Model",id='runmodel',n_clicks=0,className='me-2')),
-       html.Div(id='outputstate',children=[]),
-        html.Hr(),
-        html.Div([html.H1('',style={'textAlign': 'center'})])])
-])])])
-@app.callback(
-    Output("time-series-chart", "figure"),
-    Input("ticker", "value"))
-def display_time_series(ticker):
-    df = px.data.stocks()
-    fig = px.line(df, x='date', y=ticker)
-    return fig
+app.layout = html.Div(style={"fontFamily": ["Open Sans", "verdana", "arial", "sans-serif"]},children=
+    [
+        html.Div([
+            html.H1('Team 039 Project Dashboard: Home Credit Default Risk Analysis'),
+        ]),
+        html.Div(children=[dcc.Tabs([
+            dcc.Tab(label='Project Analysis', children=[
+                dcc.Graph(
+                    id='mean-decrease-in-impurity',
+                    figure=fig
+                ), ]
+            ),
+    dcc.Tab(label='Dummy Sliders', children=[
+        html.Div(
+            html.H6(
+                'Here you can put the undersampling model to the test. There are three features below that you can manipulate and see how they effect your liklihood of defaulting. Try putting in your numbers and see how it goes! \nThe median values from the dataset are displayed as placeholders for reference.'
+            )
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        (
+                            html.Div(children=[
+                                html.Div('AMT_CREDIT: Credit amount of the loan (must be positive)'),
+                                html.Div(dcc.Input(
+                                    id="AMT_CREDIT", type="number", placeholder=585000, min=0
+                                ))
+                            ],
+                                className="combo-box"
+                            )),
+                        (
+                            html.Div(children=[
+                                html.Div('DAYS_BIRTH: Client\'s age in days at the time of application'),
+                                html.Div(dcc.Input(id="DAYS_BIRTH", type="number", placeholder=13883.5,min=0))
+                            ],
+                                className="combo-box"
+                            )
+                        ),
+                        (
+                            html.Div(children=[
+                                html.Div('DAYS_EMPLOYED: How many days before the application the person started current employment'),
+                                html.Div(dcc.Input(id="DAYS_EMPLOYED", type="number", placeholder=1680.5, min=0))
+                            ],
+                                className="combo-box"
+                            )
+                        ),
+                        html.Div(children=[
+                                dbc.Button("Run Model",id='runmodel',n_clicks=0,className='me-2')
+                            ], className="btn-ctn"
+                            ),
+                        html.Div(id='outputstate', children=[]),
+                    ], className="form")
+                    ],
+            className="form-ctn"
+                ),
+            ]
+        ),
+    ])])])
 
 @app.callback(
     [Output('outputstate','children')],
@@ -81,7 +129,7 @@ def display_time_series(ticker):
      State(component_id = 'DAYS_BIRTH',component_property='value'),
      State(component_id = 'DAYS_EMPLOYED',component_property='value')]
      )
-def runmodels(nclicks,AMT_CREDIT,DAYS_BIRTH,DAYS_EMPLOYED):
+def runmodels(nclicks, AMT_CREDIT, DAYS_BIRTH, DAYS_EMPLOYED):
     if AMT_CREDIT is None:
         raise PreventUpdate
     elif DAYS_BIRTH is None:
